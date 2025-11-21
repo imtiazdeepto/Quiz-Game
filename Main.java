bbd4f20ff3c3b682bee7dc5f4fcb5dda7b7f3c67
Imtiaz Deepto <74072999+imtiazdeepto@users.noreply.github.com>
2025-11-21 07:53:34 +0600

    FILE 6

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // SINGLETON PATTERN: Get instance from GameManager (FILE 5)
+        // SINGLETON PATTERN:  Get instance from GameManager (FILE 5)
         // Cannot use: new GameManager() - constructor is private
         GameManager gameManager = GameManager.getInstance();
 
